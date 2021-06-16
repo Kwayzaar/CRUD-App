ac8fc2b8164d64c86f2d45e4cf69e95fba4f7165
eric <lee.eric.03@gmail.com>
2021-06-16 16:14:11 -0600

    added styling to home page

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,6 +1,32 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
 import Airline from './Airline'
+import styled from 'styled-components'
+
+const Home = styled.div`
+  text-align: center;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+`
+const Header = styled.div`
+  padding: 100px 100px 10px 100px;
+  
+  h1 {
+    font-size: 42px;
+  }
+`
+const SubHeader = styled.div`
+  font-weight: 300;
+  font-size: 26px;
+`
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100%;
+  padding: 20px;
+`
 
 const Airlines = () => {
   const [airlines, setAirlines] = useState([])
@@ -8,31 +34,29 @@ const Airlines = () => {
   useEffect(()=> {
     //Get all airlines from API 
     //Update airlines in our state 
-
     axios.get('/api/v1/airlines.json')
-    .then( resp => {
-      setAirlines(resp.data.data)
-    })
+    .then( resp => setAirlines(resp.data.data))
     .catch( resp => console.log(resp))
   }, [airlines.length])
 
   const grid = airlines.map( item => {
-    return (<Airline 
+    return (
+    <Airline 
       key={item.attributes.name}
       attributes={item.attributes}
-      />)
+    />)
   })
 
   return (
-  <div className="Home">
-    <div className="header">
+  <Home>
+    <Header>
       <h1>OpenFlights</h1>
-      <div className="subheader">Honest, unbiased airline reviews.</div>
-    </div>
-    <div className="grid">
+      <SubHeader>Honest, unbiased airline reviews.</SubHeader>
+    </Header>
+    <Grid>
       {grid}
-    </div>
-  </div>
+    </Grid>
+  </Home>
   )
 }
 
